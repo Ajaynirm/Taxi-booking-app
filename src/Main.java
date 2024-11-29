221c01f17dc562704a97fe53e0cbf88e677d6042
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-11-29 07:54:25 +0530

    files are uploaded to github

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    //System.out.println("Choose any one\n1.Book Taxi\n2.Display Details\n3.Exit");
+      int a='b'-'d';
+        System.out.println(a);
+//        boolean loop=false;
+//
+//        while(loop){
+//            System.out.println("Choose any one\n1.Book Taxi\n2.Display Details\n3.Exit");
+//            Scanner in=new Scanner(System.in);
+//            int num=in.nextInt();
+//
+//            switch(num){
+//                case 1:
+//                    System.out.println("Enter pickup Location");
+//                    char pickup=in.next().charAt(0);
+//                    System.out.println("Enter drop Location");
+//                    String dropDummy=in.next().toUpperCase();
+//                    char drop=dropDummy.charAt(0);
+//                    System.out.println("Enter Pickup time");
+//                    int pickTime=in.nextInt();
+//                    System.out.println(TaxiBooking.booking(pickup,drop,pickTime));
+//                    break;
+//                case 2:
+//                    TaxiBooking.display();
+//                    break;
+//                case 3:
+//                    loop=false;
+//                    System.out.println("Thank You ! ");
+//                    in.close();
+//                    break;
+//
+//            }
+//        }
+    }
+}
